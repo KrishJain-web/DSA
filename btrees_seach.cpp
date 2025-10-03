2c2d024d73b2e073fc840a3c00bd0ce1536eb9b5
KrishJain-web <krishjainb24bb1016@gmail.com>
2025-10-03 12:43:48 +0530

    Create btrees_seach.cpp

---
+++ btrees_seach.cpp
@@ -0,0 +1,18 @@
+class TreeNode{
+    int n;
+    TreeNode *child[MAX_CHILDREN]; //min b/2 max b
+    int key[MAX_size];
+    bool leaf;
+};
+
+TreeNode*search(TreeNode* x , int k){
+    int i = 0;
+    while(i<x->n && x->key[i] < k){
+        i++;
+    }
+    if(k == x->key[i]){
+        return x;
+    }
+    if(leaf) return nullptr;
+    return search(x->child , k);
+}
